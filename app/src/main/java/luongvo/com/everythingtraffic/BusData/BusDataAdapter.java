e8d6bb480ef74c597d986cb0b6d22d3e3ff0cb96
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-06-22 16:42:58 +0700

    Add search function for list view of bus

--- app/src/main/java/luongvo/com/everythingtraffic/BusData/BusDataAdapter.java
+++ app/src/main/java/luongvo/com/everythingtraffic/BusData/BusDataAdapter.java
@@ -6,12 +6,15 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +29,7 @@ public class BusDataAdapter extends ArrayAdapter<routeInfo> {
     private Context context;
     private int resourceID;
     private ArrayList<routeInfo> routeInfos;
+    private ArrayList<routeInfo> searchRouteInfos;
 
     public BusDataAdapter(@NonNull Context context, @LayoutRes int resource, ArrayList<routeInfo> objects) {
         super(context, resource, objects);
@@ -43,17 +47,59 @@ public class BusDataAdapter extends ArrayAdapter<routeInfo> {
             convertView = LayoutInflater.from(this.context).inflate(resourceID, parent, false);
         }
 
-        routeInfo routeInfoItem = routeInfos.get(position);
+
         TextView busName = (TextView) convertView.findViewById(R.id.busName);
         TextView busRoute = (TextView) convertView.findViewById(R.id.busRoute);
+        routeInfo routeInfoItem = routeInfos.get(position);
 
         busName.setText("Xe số : " + routeInfoItem.getRouteId());
         busRoute.setText(routeInfoItem.getRouteName());
         return convertView;
-
-
-
-
-
     }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<routeInfo> filteredRouteInfos = new ArrayList<>();
+
+                if (searchRouteInfos == null) {
+                    searchRouteInfos = new ArrayList<>(routeInfos);
+                }
+
+                if (constraint == null || constraint.length() == 0) {
+                    results.count = searchRouteInfos.size();
+                    results.values = searchRouteInfos;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < searchRouteInfos.size(); i++) {
+                        String data = "Xe số " + searchRouteInfos.get(i).getRouteId();
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            filteredRouteInfos.add(searchRouteInfos.get(i));
+                        }
+                    }
+                    results.count = filteredRouteInfos.size();
+                    results.values = filteredRouteInfos;
+                }
+                return results;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                routeInfos = (ArrayList<routeInfo>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
+    }
+
+    @Override
+    public int getCount() {
+        return routeInfos.size();
+    }
+
 }
--- app/src/main/java/luongvo/com/everythingtraffic/BusInfoActivity.java
+++ app/src/main/java/luongvo/com/everythingtraffic/BusInfoActivity.java
@@ -4,8 +4,11 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -29,6 +32,7 @@ public class BusInfoActivity extends AppCompatActivity{
     private StringBuffer postList;
     private ListView listView;
     BusDataAdapter busDataAdapter;
+    private EditText typeToSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,13 +95,6 @@ public class BusInfoActivity extends AppCompatActivity{
 //                txtPostList.setText(postList);
                 busDataAdapter = new BusDataAdapter(BusInfoActivity.this, R.layout.bus_item, routeInfoList);
                 listView.setAdapter(busDataAdapter);
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        routeInfo item = routeInfoList.get(position);
-                        // work to be done here
-                    }
-                });
                 progressDialog.dismiss();
             }
         }.execute();
@@ -107,6 +104,32 @@ public class BusInfoActivity extends AppCompatActivity{
 
     private void initComponent() {
         listView = (ListView) findViewById(R.id.listOfBus);
+        typeToSearch = (EditText) findViewById(R.id.typeToSearch);
+
+        typeToSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                BusInfoActivity.this.busDataAdapter.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                routeInfo item = routeInfoList.get(position);
+                // work to be done here
+            }
+        });
     }
 
 
